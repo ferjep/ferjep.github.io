d166285124c136a75fc95f7c9db11c3d2610f29e
Fernando <fernando.fjep@gmail.com>
2023-07-22 18:13:08 -0400

    color changes to sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -10,7 +10,7 @@ const props = defineProps({
 </script>
 <template>
     <div class="h-screen w-64">
-        <div class="fixed w-64 h-full bg-gray-800 py-4 flex flex-col justify-center items-center shadow-xl text-white">
+        <div class="fixed w-64 h-full bg-slate-800 py-4 flex flex-col justify-center items-center shadow-xl text-white">
             <h1 class="text-2xl text-center italic px-4">
                 Fernando Esparragoza
             </h1>
--- src/components/SideBarLink.vue
+++ src/components/SideBarLink.vue
@@ -11,10 +11,8 @@ const props = defineProps({
     <li class="mb-2 last:mb-0">
         <a
             :href="'#' + props.href"
+            :class="{'border-white bg-gray-700': props.active}"
             class="flex items-center py-2 px-4 border-transparent border-b hover:border-white hover:bg-gray-700"
-            :class="{
-                'border-white bg-gray-700': props.active,
-            }"
         >
             <img class="mr-2 w-5 h-5" :src="props.icon" />
             <span>{{ props.title }}</span>
