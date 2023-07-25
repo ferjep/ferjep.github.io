41f1c61b07a899070445b6ac98535a34aca6cae7
Fernando <fernando.fjep@gmail.com>
2023-07-25 00:59:28 -0400

    Experience view

--- src/components/ProjectsView.vue
+++ src/components/ProjectsView.vue
@@ -1,15 +1,73 @@
 <script setup>
 import SectionView from './SectionView.vue'
+import MintmeLogo from '../assets/icons/mintme.png'
+import BgText from './BgText.vue'
+import skills from '../data/skills'
 
 const props = defineProps({id: String })
 
 </script>
 <template>
     <SectionView :id="props.id">
-        <div class="h-full w-full flex justify-center items-center bg-gray-500">
-            <h1 class="text-2xl">
-                Projects
-            </h1>
+        <div class="h-full w-full flex justify-center items-center">
+            <div>
+                <div class="shadow-md p-3 mb-4 rounded bg-gray-200 mb-5">
+                    <div class="flex items-center mb-2">
+                        <div class="w-28 mr-2">
+                            <a href="https://www.mintme.com" target="_blank">
+                                <img :src="MintmeLogo" class="h-full"/>
+                            </a>
+                        </div>
+                        <div class="w-full">
+                            <h1 class="text-xl w-full mb-1">
+                                <BgText>
+                                    Senior Full-Stack Developer
+                                </BgText>
+                                <a href="https://www.mintme.com" target="_blank" class="inline-block float-right">
+                                    &#128279;
+                                </a>
+                            </h1>
+                            <p class="text-base mb-1">
+                                <a href="https://www.mintme.com" target="_blank" class="inline-block underline">
+                                    MintMe.com
+                                </a>
+                                · Full-time
+                            </p>
+                            <p class="text-base text-xs mb-1">
+                                Feb 2021 · Jun 2021 
+                            </p>
+                            <p class="text-base text-xs mb-1">
+                                Belize · Remote
+                            </p>
+                        </div>
+                    </div>
+                    <p class="text-sm text-justify leading-relaxed">
+                        In my current role as a Full-stack Developer, I've focused on optimizing frontend assets for improved loading times on Google PageSpeed Insights. I've developed various features and fixes using Symfony and Vue.js, managed database migrations, and completely refactored the blockchain gateway using Typescript and Node.js while ensuring backward compatibility. Additionally, I handle the maintenance and implementation of Docker containers, and actively mentor and review other developers to enhance our platform's codebase and services workflow.
+                    </p>
+                </div>
+                <div>
+                    <p class="text-center mb-4 underline">
+                        Skills
+                    </p>
+                    <div class="grid grid-cols-3 gap-4 mb-4">
+                        <div v-for="skill in skills" :key="skill.name">
+                            <div class="flex items-center justify-center text-white bg-slate-900 px-1 py-2 rounded h-12">
+                                <img :src="skill.icon" class="h-full mr-2"/>
+                                <p class="text-base">{{skill.name}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <a
+                            class="z-50 text-lg hover:underline rounded inline-block bg-emerald-700 hover:bg-emerald-900 text-white px-2 cursor-pointer"
+                            href="https://drive.google.com/file/d/1_xAWXTVZJQuYLwS07YdFee5rq17gBSDM/view?usp=sharing"
+                            target="_blank"
+                        >
+                            View full resume
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </SectionView>
 </template>
---
+++ src/data/skills.js
@@ -0,0 +1,49 @@
+import JavaScriptIcon from '../assets/icons/javascript.svg'
+import TypeScriptIcon from '../assets/icons/typescript.svg'
+import PythonIcon from '../assets/icons/python.svg'
+import NodeIcon from '../assets/icons/nodejs.svg'
+import PhpIcon from '../assets/icons/php.svg'
+import SymfonyIcon from '../assets/icons/symfony.svg'
+import DockerIcon from '../assets/icons/docker.svg'
+import VueIcon from '../assets/icons/vuejs.svg'
+import ReactIcon from '../assets/icons/react.svg'
+
+export default [
+    {
+        name: 'JavaScript',
+        icon: JavaScriptIcon
+    },
+    {
+        name: 'TypeScript',
+        icon: TypeScriptIcon
+    },
+    {
+        name: 'Python',
+        icon: PythonIcon,
+    },
+    {
+        name: 'NodeJs',
+        icon: NodeIcon,
+    },
+    {
+        name: 'Php',
+        icon: PhpIcon,
+    },
+    {
+        name: 'Symfony',
+        icon: SymfonyIcon,
+    },
+    {
+        name: 'VueJs',
+        icon: VueIcon,
+
+    },
+    {
+        name: 'ReactJs',
+        icon: ReactIcon,
+    },
+    {
+        name: 'Docker',
+        icon: DockerIcon,
+    }
+]
