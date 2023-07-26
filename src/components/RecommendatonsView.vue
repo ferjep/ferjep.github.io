2166031a6bfe48f00f45b0ab0b6e5bc4f2a6654e
Fernando <fernando.fjep@gmail.com>
2023-07-26 00:14:31 -0400

    Finishing recommendations view

--- src/components/RecommendatonsView.vue
+++ src/components/RecommendatonsView.vue
@@ -1,14 +1,63 @@
 <script setup>
 import SectionView from './SectionView.vue'
+import BgText from './BgText.vue'
+import recommendations from '../data/recommendations'
+import { ref } from 'vue';
 
 const props = defineProps({id: String})
+
+const currentIndex = ref(0)
+const container = ref(null)
+
+const scrollTo = (index) => {
+    container.value.scrollTo({
+        top: 0,
+        left: container.value.offsetWidth * index,
+        behavior: 'smooth'
+    })
+
+    currentIndex.value = index
+}
 </script>
 <template>
     <SectionView :id="props.id">
-        <div class="h-full w-full flex justify-center items-center bg-yellow-500">
-            <h1 class="text-2xl">
-                recommendations
-            </h1>
+        <div class="h-full w-full flex flex-col justify-center">
+            <div ref="container" class="flex items-center gap-8 overflow-x-hidden overflow-y-hidden snap-x mb-4">
+                <div v-for="recommendation in recommendations" :key="recommendation.name" class="snap-center shrink-0 w-full">
+                    <div class="w-full bg-gray-200 p-4 rounded shadow-md relative">
+                        <a :href="recommendation.profileUrl" target="_blank" class="absolute right-0 mr-4">
+                            &#128279;
+                        </a>
+                        <div class="flex flex-col items-center mb-4">
+                            <a :href="recommendation.profileUrl" target="_blank">
+                                <img :src="recommendation.profileImg" class="rounded-md w-20 mb-2"/>
+                            </a>
+                            <BgText class="mb-2">
+                                <a :href="recommendation.profileUrl" target="_blank">{{recommendation.name}}</a>
+                            </BgText>
+                            <p class="text-base">{{recommendation.role}}</p>
+                        </div>
+                        <p class="text-sm text-justify italic">
+                            <span class="text-4xl">&#10077;</span>
+                            {{recommendation.text}}
+                            <span class="text-4xl float-right">&#10078;</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <button
+                    v-for="index in recommendations.length"
+                    class="w-8 h-8 rounded-full mr-2"
+                    :key="index"
+                    :class="{
+                        'bg-slate-900': currentIndex === index - 1,
+                        'border border-slate-700 border-4': currentIndex !== index -1
+                    }"
+                    @click="scrollTo(index - 1)"
+                >
+                </button>
+            </div>
         </div>
     </SectionView>
 </template>
--- src/data/recommendations.js
+++ src/data/recommendations.js
@@ -1,5 +1,43 @@
 export default [
     {
-
+        name: 'Rodolfo Hernández',
+        role: 'DevOps/System Administrator',
+        text: 'I worked with Fernando in a cryptocurrency project for about 2 years and I can say that you will never be disappointed about his skills (soft and hard). It is very creative and accurate in complex situations,  always has something to give and propose, and technically is simply outstanding. Who knows me, knows that I have many years of experience in large, mid, and startup companies, and with no doubt I could say that Fernando is one of the more impressive programmer that I have co worked with.',
+        profileUrl: 'https://www.linkedin.com/in/rodolfo-hernandez-8b427350/',
+        profileImg: 'https://media.licdn.com/dms/image/D4D03AQEUyNWh4lI-3g/profile-displayphoto-shrink_200_200/0/1667558713608?e=1695859200&v=beta&t=itdjvARX4IjqERHSfbSStoSGTQWBKAv9RFZbU3rdI-U'
+    },
+    {
+        name: 'Gustavo Diaz',
+        role: 'Software Enginer',
+        text: 'Fernando is an excellent software developer with all the needed skills to work on any development concerns, so I recommend him as a key piece for any company.',
+        profileUrl: 'https://www.linkedin.com/in/gustavo-diaz-15a85a43/',
+        profileImg: 'https://media.licdn.com/dms/image/C5603AQEhqmaWAoliUQ/profile-displayphoto-shrink_200_200/0/1652389206837?e=1695859200&v=beta&t=G4IU8FBnzh64EUbdPQIF0hC6B_Y6djk0vwMixM68nIQ'
+    },
+    {
+        name: 'Basil Kravtsov',
+        role: 'Software Engineer',
+        text: 'Fernando was one of the three elephants on which our world  "minmte.com" stand on. Very strong blockchain technology expertise, great communication skills and perfect sense of humor. He capable to fix two bugs in same time - one for each hand. I would like to work with Fernando once again some day.',
+        profileUrl: 'https://www.linkedin.com/in/basilkravtsov/',
+        profileImg: 'https://media.licdn.com/dms/image/D5603AQG51Er2XTIacg/profile-displayphoto-shrink_200_200/0/1680705983812?e=1695859200&v=beta&t=SyUJt0PhnSz_hqXgSJEAi0Uttj0wfIrx-Sb99EK_i1M'
+    },
+    {
+        name: 'Osama Hussien',
+        role: 'Backend Enginer',
+        text: 'I had the pleasure of working closely with Fernando during our time together as software engineers at MintMe, and I can wholeheartedly vouch for his exceptional skills and contributions. Fernando is an outstanding developer who consistently delivers high-quality work and demonstrates a deep understanding of software engineering principles. He tackles complex challenges with ease, offering innovative solutions that have significantly improved our team\'s efficiency. Beyond his technical abilities, Fernando is a true team player, always willing to lend a helping hand and provide guidance to his colleagues. His collaborative nature and strong communication skills make him a valuable asset to any project. I have the utmost confidence in Fernando\'s abilities and believe he would be a valuable addition to any software engineering team.',
+        profileUrl: 'https://www.linkedin.com/in/osama-hussien/'
+    },
+    {
+        name: 'Cristian Rafael',
+        role: 'Web Developer',
+        text: 'Un gran hombre con mente muy lógica, con humor cuando necesita y gran ingenio para resolver problemas cuando es necesario pensar fuera de la caja,  colaborador cuando se trata de trabajar en equipo; las veces que necesitaba ayuda Fernando siempre me brindaba su ayuda. En conclusión es una excelente persona tanto a nivel de amistad como a nivel laboral',
+        profileUrl: 'https://www.linkedin.com/in/cristian-rafael-6092a2193',
+        profileImg: 'https://media.licdn.com/dms/image/D4E03AQH4ENxm5VGoHg/profile-displayphoto-shrink_200_200/0/1667083547906?e=1695859200&v=beta&t=X3F2tqlfKfG9LR77XXggWN1jSsBplQwJBY3nnEXP26Q'
+    },
+    {
+        name: 'Min Thu Kyaw',
+        role: 'Software Enginer',
+        text: 'Fernando was my best colleague in my previous job. He\'s an awesome developer who knows everything about servers, docker containers, pipelines, web3 blockchains, and also writing codes. I would recommend him 11 out of 10. If possible, I would like to work with Fernando again.',
+        profileUrl: 'https://www.linkedin.com/in/mtk246/',
+        profileImg: 'https://media.licdn.com/dms/image/D5635AQEXH8-DEiOckQ/profile-framedphoto-shrink_200_200/0/1687520117974?e=1690938000&v=beta&t=QoaR1V4SA1MVl1LLSNDF0HMXwq340rYkH8vb-xK0Iqc'
     }
 ]
